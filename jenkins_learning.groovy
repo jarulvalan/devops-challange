ef9a4221d38763469deef795508605dbdf55d737
jarulvalan <jarulvalan@gmail.com>
2020-01-31 13:41:19 +0530

    Add files via upload

---
+++ jenkins_learning.groovy
@@ -0,0 +1,35 @@
+
+def devops_git='https://github.com/jarulvalan/devops-challange'
+
+freeStyleJob("DevOps-Challenge")
+{
+    logRotator(2,5)
+    parameters{
+        stringParam("simple input")
+    }
+    label("test")
+    scm
+    {
+        git($devops_git, "master")
+    }
+    steps{
+        sh echo "Running the steps1"
+    }
+    publishers
+    {
+        extendedEmail
+        {
+            recipientList()
+            defalutSubject("Job failed")
+            ddefaultContent("$BUILD_URL")
+            triggers
+            {
+                aborted
+                {
+                    subject("Job aborted $JOB_NAME")
+                }
+            }
+        }
+    }
+}
+
